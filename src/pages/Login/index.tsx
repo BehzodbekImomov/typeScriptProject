d4ac1abd002c731a4b0474ce700519e5e568a955
Imomov Behzodbek <behzodbeki65@gmail.com>
2025-07-27 16:32:09 +0500

    img changes

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,13 +1,14 @@
 import React, { Fragment } from "react";
 import SignIn from "../../components/SignIn";
 import styles from "./LoginP.module.css";
+import typescript from "../../../src/assets/svgs/typescript.svg"
 
 const LoginP: React.FC = () => {
   return (
     <Fragment>
       <div className="container flex">
         <div className={styles.card}>
-          <img src="../../src/assets/svgs/typescript.svg" alt="TypeScript Logo" />
+          <img src={typescript} alt="TypeScript Logo" />
         </div>
         <SignIn />
       </div>
